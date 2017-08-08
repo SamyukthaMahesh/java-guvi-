b9a3a920c8be8cad0cfbbc1052e77cee75358b24
Samyuktha Mahesh <samyukthamahesh@gmail.com>
2017-08-08 23:41:19 +0530

    Create EvenOdd.java
    It is a program for finding a number is even or odd

---
+++ EvenOdd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x;
+System.out.println("Enter the number");
+Scanner in = new Scanner(System.in);
+x = in.nextInt();
+if (x%2==0)
+{
+	System.out.println("Even");
+}
+else
+{
+	System.out.println("Odd");
+}
+	}
+
+}
